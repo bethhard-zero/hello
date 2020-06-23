4aa6df1de447d2e78b5ef2219ec5c08b2de962ba
jianwei.cai <jianwei.cai@luckincoffee.com>
2020-06-24 00:28:01 +0800

    init

---
+++ src/main/java/com/bee/hello/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.bee.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Taiga Cai
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+
+}
